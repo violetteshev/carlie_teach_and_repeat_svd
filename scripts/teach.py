c7393d874a89419c6f0870d0894882df62c1fd15
James Mount <j.mount@qut.edu.au>
2020-11-17 15:44:41 +1000

    Initial Commit. Includes initial teach node.

---
+++ scripts/teach.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+### IMPORT CLASSES ###
+import os
+import rospy
+import shutil
+import cv2 as cv
+import tf_conversions
+from cv_bridge import CvBridge
+from tf.transformations import euler_from_quaternion
+
+### IMPORT MESSAGE TYPES ###
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
+from nav_msgs.msg import Odometry
+
+
+### TEACH NODE CLASS ###
+class TeachNode():
+
+    # INITIALISATION
+    def __init__(self):
+        # Parameters
+        self.frame_id = 0
+        self.previous_odom = None # odometry pose of previous image
+        self.current_odom = None # odometry pose of current image
+
+        # ROS Init Node
+        rospy.init_node('teach_node')
+        rospy.loginfo("Teach Node Initialised")
+
+        # Constants
+        self.KEYFRAME_DISTANCE_THRESHOLD = rospy.get_param('~keyframe_distance_threshold', 0.25)
+        self.IMAGE_RESIZE = (rospy.get_param('~image_resize_x', 640), rospy.get_param('~image_resize_y', 480))
+        self.BASE_PATH = rospy.get_param('~base_path', '/home/james/Documents')
+        self.ROUTE_NAME = rospy.get_param('~route_name', 'route_1')
+        self.CV_BRIDGE = CvBridge()
+
+        # Setup save directory and dataset file
+        self.save_path = os.path.join(self.BASE_PATH, self.ROUTE_NAME)
+        if os.path.exists(self.save_path):
+            shutil.rmtree(self.save_path) # will delete existing save_path directory and its contents
+        os.makedirs(self.save_path)
+        self.dataset_file = open(os.path.join(self.save_path, 'dataset.txt'), 'w')
+        self.dataset_file.write("Frame_ID, relative_x(m), relative_y(m), relative_yaw(rad)\n")
+
+        # ROS Subcribers
+        self.odom_subscriber = rospy.Subscriber('odom', Odometry, self.Odom_Callback)
+        self.image_subscriber = rospy.Subscriber('image_raw', Image, self.Image_Callback)
+
+        # ROS Spin
+        while not rospy.is_shutdown():
+            pass
+
+    # ODOM CALLBACK
+    def Odom_Callback(self, data):
+        self.current_odom = data
+
+        # set previous odom if has not already been set
+        if self.previous_odom == None:
+            self.previous_odom = data
+
+    # IMAGE CALLBACK
+    def Image_Callback(self, data):
+        if self.current_odom == None or self.previous_odom == None:
+            rospy.logwarn('Unable to get relative pose transform. Make sure odometry topic is published and the teach node is subscribed to the correct topic.')
+            return # safeguard
+
+        # Relative odometry        
+        current_odom_tf = tf_conversions.fromMsg(self.current_odom.pose.pose)
+        previous_odom_tf = tf_conversions.fromMsg(self.previous_odom.pose.pose)
+        relative_odom_tf = previous_odom_tf.Inverse() * current_odom_tf
+        relative_pose = tf_conversions.toMsg(relative_odom_tf)
+
+        if relative_odom_tf.p.Norm() < self.KEYFRAME_DISTANCE_THRESHOLD and self.frame_id >= 1:
+            return
+
+        # Attempt to convert ROS image into CV data type (i.e. numpy array)
+        try:
+            img_bgr = self.CV_BRIDGE.imgmsg_to_cv2(data, "bgr8")
+        except Exception as e:
+            rospy.logerr("Unable to convert ROS image into CV data. Error: " + str(e))
+            return
+
+        # Resize Image
+        img_bgr = cv.resize(img_bgr, self.IMAGE_RESIZE)
+
+        # Save Image and Relative Odometry
+        frame_name = "frame_%06d.png" % self.frame_id
+        try:
+            cv.imwrite(os.path.join(self.save_path, frame_name), img_bgr)
+        except Exception as e:
+            rospy.logwarn("Was unable to save image. Error: " + str(e))
+            return
+
+        # Write to dataset file
+        (roll, pitch, yaw) = euler_from_quaternion([relative_pose.orientation.x, relative_pose.orientation.y, relative_pose.orientation.z, relative_pose.orientation.w])
+        self.dataset_file.write('%s, %0.4f, %0.4f, %0.4f\n'%(frame_name, relative_pose.position.x, relative_pose.position.y, yaw))
+
+        # Update frame ID and previous odom
+        self.frame_id += 1
+        self.previous_odom = self.current_odom
+
+
+### MAIN ####
+if __name__ == "__main__":
+    try:
+        teach = TeachNode()
+    except rospy.ROSInterruptException:
+        teach.dataset_file.close()
+
+    teach.dataset_file.close()
